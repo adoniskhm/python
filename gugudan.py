db75532976c113e41c6231a6b2e04d1f1b7bdae9
Kwon Hyeok Min <adoniskhm@gmail.com>
2024-09-30 19:19:33 +0800

    파이썬의자료형

--- gugudan.py
+++ gugudan.py
@@ -1,4 +1,4 @@
-print(" * 구구단 * ")
+print(" * 구구단 *  ")
 for i in range(2, 10):
     print(f"\n<< {i}단 >>")
     for j in range(2, 10):
---
+++ num.py
@@ -0,0 +1,3 @@
+print(type(123)) #정수형
+print(type(1.23)) #실수형
+print(type("123")) #문자열

9e1ac5ee7e94fc2d7b612dce575db28ac27575d2
Kwon Hyeok Min <adoniskhm@gmail.com>
2024-10-06 12:24:01 +0800

    class

---
+++ class.py
@@ -0,0 +1,22 @@
+# class Cat:
+#     def meow(self):
+#         print("야옹~~")
+
+# Cat
+
+class Korean :
+    nation = "대한민국"
+    def sayHello(self) :
+        print("안녕하세요!")
+
+class Chinese :
+    def sayHello(self, name) :
+        print("你好" + name)
+
+k = Korean()
+c = Chinese()
+
+k.sayHello()
+c.sayHello("我是权赫民")
+
+print(k.nation)
